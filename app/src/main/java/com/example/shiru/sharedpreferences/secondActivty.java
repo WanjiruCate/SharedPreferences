37b4b7757c678ddc70a2d1dbe9dc4dd6dc319e73
kateshiru <catherinemunene802@gmail.com>
2018-10-22 14:13:10 +0300

    Added clear and remove functionality to shared preferences

--- app/src/main/java/com/example/shiru/sharedpreferences/secondActivty.java
+++ app/src/main/java/com/example/shiru/sharedpreferences/secondActivty.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class secondActivty extends AppCompatActivity {
 
@@ -32,9 +33,26 @@ public class secondActivty extends AppCompatActivity {
         txtSecondCity.setText(city);
     }
 
+    //Delete all account data from shared preferences
     public void clearAccountData(View view) {
+
+        SharedPreferences preferences = getSharedPreferences(getPackageName()+Constants.PREF_FILE_NAME,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.clear();
+        editor.apply();
+
+        Toast.makeText(getApplicationContext(),"Cleared shared Preferences",Toast.LENGTH_LONG).show();
     }
 
     public void removeProffessionKey(View view) {
+
+        SharedPreferences preferences = getSharedPreferences(getPackageName()+Constants.PREF_FILE_NAME,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.remove(Constants.KEY_CITY);
+        editor.apply();
+
+        Toast.makeText(getApplicationContext(),"Removed"+Constants.KEY_CITY+ " shared Preferences",Toast.LENGTH_LONG).show();
     }
 }
